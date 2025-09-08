e7a060902f69c294c907a20c89dd5d3544c0d3b2
Rutger van Haasteren <vhaasteren@gmail.com>
2025-09-08 10:47:08 +0200

    Final version

--- build_roc.sh
+++ build_roc.sh
@@ -5,4 +5,4 @@ python ./build_npcc_roc_json.py \
   --fig-out genx2-figure-data.json \
   --grid-log 1e-8 0.9999999 1200 \
   --grid-lin 1e-9 1.0 1200 \
-  --x-min -12 --x-max 24 --nx 600
+  --x-min -24 --x-max 24 --nx 800
